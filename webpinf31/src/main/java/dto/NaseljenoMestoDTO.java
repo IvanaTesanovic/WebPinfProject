aa35a16be670a1739681ad3fa7d8c966ccd1f718
SnezanaStevanovic <snezastevanovic@gmail.com>
2016-06-29 01:03:39 +0200

    ...

---
+++ webpinf31/src/main/java/dto/NaseljenoMestoDTO.java
@@ -0,0 +1,46 @@
+package dto;
+
+
+import model.Drzava;
+
+public class NaseljenoMestoDTO {
+	public String naziv;
+	public String ptt_oznaka;
+	public String id_drzave;
+	
+	public NaseljenoMestoDTO() { }
+
+	public NaseljenoMestoDTO(String naziv, String ptt_oznaka, String id_drzave) {
+		super();
+		this.naziv = naziv;
+		this.ptt_oznaka = ptt_oznaka;
+		this.id_drzave = id_drzave;
+	}
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public String getPtt_oznaka() {
+		return ptt_oznaka;
+	}
+
+	public void setPtt_oznaka(String ptt_oznaka) {
+		this.ptt_oznaka = ptt_oznaka;
+	}
+
+	public String getId_drzave() {
+		return id_drzave;
+	}
+
+	public void setId_drzave(String id_drzave) {
+		this.id_drzave = id_drzave;
+	}
+	
+
+	
+}
---
+++ webpinf31/src/main/java/repository/VrstePlacanjaRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import model.VrstePlacanja;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VrstePlacanjaRepository extends CrudRepository<VrstePlacanja, Long> {
+
+}
