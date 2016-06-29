8ff05a2489922a2258fb16b11cf1c848bca3ff53
SnezanaStevanovic <snezastevanovic@gmail.com>
2016-06-29 02:08:17 +0200

    Update funkcije

--- webpinf31/src/main/java/service/KliringService.java
+++ webpinf31/src/main/java/service/KliringService.java
@@ -1,7 +1,9 @@
 package service;
 
+import java.util.Date;
 import java.util.List;
 
+import model.GrupaIzvoda;
 import model.Kliring;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,5 +33,27 @@ public class KliringService {
 	
 	 public Kliring findById(Long id){
 		 return kliringRepo.findOne(id);
+	 }
+	 
+	 public void update(Long id, String id_poruke, String swift_banke_duznika,
+				String obracunski_racun_banke_duznika,
+				String swift_banke_poverioca,
+				String obracunski_racun_banke_poverioca, Date datum_naloga,
+				String id_valute, Date datum_valute, Double ukupan_iznos,
+				GrupaIzvoda id_grupe_izvoda){
+		 
+		 
+		 Kliring kliring = kliringRepo.findOne(id);
+		 kliring.setId_poruke(id_poruke);
+		 kliring.setSwift_banke_duznika(swift_banke_duznika);
+		 kliring.setObracunski_racun_banke_duznika(obracunski_racun_banke_duznika);
+		 kliring.setSwift_banke_poverioca(swift_banke_poverioca);
+		 kliring.setObracunski_racun_banke_poverioca(obracunski_racun_banke_poverioca);
+		 kliring.setDatum_naloga(datum_naloga);
+		 kliring.setId_valute(id_valute);
+		 kliring.setDatum_valute(datum_valute);
+		 kliring.setUkupan_iznos(ukupan_iznos);
+		 kliring.setId_grupe_izvoda(id_grupe_izvoda);
+		 kliringRepo.save(kliring);
 	 }
 }
--- webpinf31/src/main/java/service/RacunService.java
+++ webpinf31/src/main/java/service/RacunService.java
@@ -1,8 +1,12 @@
 package service;
 
+import java.sql.Date;
 import java.util.List;
 
+import model.Banka;
+import model.Klijent;
 import model.Racun;
+import model.Valuta;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,5 +35,17 @@ public class RacunService {
 
 	 public Racun findById(Long id){
 		 return racunRepo.findOne(id);
+	 }
+	 public void update(Long id, String broj_racuna, Date datum_otvaranja, Boolean vazeci,
+			Banka id_banke, Valuta id_valute, Klijent id_klijenta){
+		 
+		 Racun racun = racunRepo.findOne(id);
+		 racun.setBroj_racuna(broj_racuna);
+		 racun.setDatum_otvaranja(datum_otvaranja);
+		 racun.setVazeci(vazeci);
+		 racun.setId_banke(id_banke);
+		 racun.setId_valute(id_valute);
+		 racun.setId_klijenta(id_klijenta);
+		 racunRepo.save(racun);
 	 }
 }
